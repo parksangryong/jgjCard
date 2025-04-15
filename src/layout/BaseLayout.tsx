8d6674cc0b70fea0b7526c3fff72369a8ae9f229
parksangryong <ppsr3478@naver.com>
2025-04-15 13:03:06 +0900

    copyright

--- src/layout/BaseLayout.tsx
+++ src/layout/BaseLayout.tsx
@@ -1,9 +1,14 @@
 import { Outlet } from "react-router-dom";
 
 const BaseLayout = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <div className="layout-container">
       <Outlet />
+      <span className="copyright">
+        © {currentYear} Memomo. Mirr56 All rights reserved. 2025
+      </span>
     </div>
   );
 };
